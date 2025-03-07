3570ee291bf4268aa0d3026f36f33c102699ca14
Kritica <kriticaarora03@gmail.com>
2025-03-07 19:54:15 +0530

    done changes

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -2,7 +2,10 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        message: JSON.stringify('Hello from Lambda!'),
+        message: "Hello from Lambda!",
+        statusCode: 400,
+        message: "Bad request syntax or unsupported method. Request path: {path}. HTTP method: {method}"
+        
     };
     return response;
 };
