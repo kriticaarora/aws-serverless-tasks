aae09aee14995fe530b374762842df4733c956c6
Kritica <kriticaarora03@gmail.com>
2025-03-08 03:23:23 +0530

    done final editing of task02

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,11 +1,25 @@
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
+    const path = event.rawPath || event.path;
+    const method = event.requestContext?.http?.method || event.httpMethod;
+     
+    if (path === "/hello" && method === "GET") {
+    return {
         statusCode: 200,
-        message: "Hello from Lambda!",
-        statusCode: 400,
-        message: "Bad request syntax or unsupported method. Request path: {path}. HTTP method: {method}"
-        
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+        statusCode: 200,
+        message: "Hello from Lambda"
+        }),
     };
-    return response;
+    } 
+    else {
+    return {
+    statusCode: 400,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+    statusCode: 400,
+    message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`,
+    }),
+    };
+    }
 };
