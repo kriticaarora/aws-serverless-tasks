6435f80ee1febc4c77f5e0345dc7ecdeb87d4519
Kritica <kriticaarora03@gmail.com>
2025-03-12 15:16:45 +0530

    done final editing on task03

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -2,8 +2,8 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         
-            "statusCode": 200,
-            "message": "Hello from Lambda"
+        "statusCode": 200,
+        "message": "Hello from Lambda"
         
     };
     return response;
